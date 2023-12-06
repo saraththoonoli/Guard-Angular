b5cc5fd43fbca11dbfda2d3635c4760c30465bbf
sarathThoonoli <thoonolisarath@gmail.com>
2023-12-06 16:56:10 +0530

    commited

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CanComponentDeactivate } from '../can-deactivate.guard';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements CanComponentDeactivate {
+  submitted = false;
+
+  submitForm() {
+    this.submitted = true;
+    console.log('Form submitted!');
+  }
+
+  canDeactivate(): boolean {
+    if (!this.submitted) {
+      return window.confirm('Do you really want to leave without submitting the form?');
+    }
+    
+    return true;
+  }
+}
